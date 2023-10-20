50bec4b8599e4e023ecfccfea4b4e2ef728d4872
Олег <shuwiku@gmail.com>
2023-10-21 02:37:24 +0700

    Upload Lab-No1 files

---
+++ Lab-No1/script/calculator.js
@@ -0,0 +1,48 @@
+function addition() { 
+    // Запрос чисел у пользователя, преобразование в тип float
+    let num1 = parseFloat(prompt('Введите первое число: '));
+    let num2 = parseFloat(prompt('Введите второе число: '));
+    // Вывод результата
+    let output = `Результат: ${num1} + ${num2} = ${num1 + num2}`;
+    alert(output);
+}
+
+
+function division() {
+    // Запрос чисел у пользователя, преобразование в тип float
+    let num1 = parseFloat(prompt('Введите первое число: '));
+    let num2 = parseFloat(prompt('Введите второе число: '));
+    // Вывод результата
+    let output = `Результат: ${num1} / ${num2} = ${num1 / num2}`;
+    alert(output);
+}
+
+
+function multiplication() {
+    // Запрос чисел у пользователя, преобразование в тип float
+    let num1 = parseFloat(prompt('Введите первое число: '));
+    let num2 = parseFloat(prompt('Введите второе число: '));
+    // Вывод результата
+    let output = `Результат: ${num1} * ${num2} = ${num1 * num2}`;
+    alert(output);
+}
+
+
+function remainder() {
+    // Запрос чисел у пользователя, преобразование в тип float
+    let num1 = parseFloat(prompt('Введите первое число: '));
+    let num2 = parseFloat(prompt('Введите второе число: '));
+    // Вывод результата
+    let output = `Результат: ${num1} % ${num2} = ${num1 % num2}`;
+    alert(output);
+}
+
+
+function subtraction() {
+    // Запрос чисел у пользователя, преобразование в тип float
+    let num1 = parseFloat(prompt('Введите первое число: '));
+    let num2 = parseFloat(prompt('Введите второе число: '));
+    // Вывод результата
+    let output = `Результат: ${num1} - ${num2} = ${num1 - num2}`;
+    alert(output);
+}
